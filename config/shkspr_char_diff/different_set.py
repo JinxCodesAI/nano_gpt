32ee91961fe77e1449eedcffc16f5e47a46d9e3e
Adam Skrodzki <adam@everdome.io>
2025-08-30 08:02:06 +0200

    new training set

---
+++ config/shkspr_char_diff/different_set.py
@@ -0,0 +1,32 @@
+out_dir = 'out_exp'
+init_from = 'resume' # 'scratch' or 'resume'
+wandb_log = True # disabled by default
+wandb_project = 'experiments_diffusion'
+wandb_run_name = 'shkspr_char_diff_moderate_first' # 'run' + str(time.time())
+# data
+dataset = 'shakespeare_char'
+use_paragraph_boundaries = False # if True, start samples at paragraph boundaries (double newlines)
+# diffusion training config
+training_type = 'unmasking'  # 'unmasking', 'remasking', or 'remasking_binary' - type of training
+
+# For unmasking: stage-based training with direct probability control
+
+unmasking_stages = [
+    {'type':'sticky','target_masked_ratio': 0.4, 'p1_probability': 0.15, 'p2_probability': 0.3, 'val_loss_stale_count': 6},
+    {'type':'sticky','target_masked_ratio': 0.6, 'p1_probability': 0.1, 'p2_probability': 0.5, 'val_loss_stale_count': 8},
+    {'type':'sticky','target_masked_ratio': 0.6, 'p1_probability': 0.2, 'p2_probability': 0.1, 'val_loss_stale_count': 8},
+    {'type':'random','max_masked_ratio': 0.7, 'val_loss_stale_count': 10}
+]
+
+# adamw optimizer
+learning_rate = 5e-4 # with baby networks can afford to go a bit higher
+max_iters = 50000
+warmup_iters = 1000 # how many steps to warm up for
+lr_decay_iters = 50000 # it's just experiment, no need to decay
+min_lr = 1e-4 # learning_rate / 10 usually
+weight_decay=1e-3
+dropout = 0.01 # for pretraining 0 is good, for finetuning try 0.1+
+
+grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
+# learning rate decay settings
+decay_lr = False # it's just experiment, no need to decay
